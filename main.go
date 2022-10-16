299b2ab0bea51bbd27b6d2f7fb7b62c3148cfe81
Aksh Bansal <akshmark@gmail.com>
2022-10-16 21:07:08 +0530

    some changes

--- main.go
+++ main.go
@@ -3,20 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
-
-func main() {
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("♪ ")
-		inp, _, _ := reader.ReadLine()
-		tokens := lexer(string(inp))
-		// fmt.Printf("%+v", tokens)
-		fmt.Println(tokens)
-	}
-
-}
 
 const (
 	mul = "MUL"
@@ -29,6 +18,20 @@ const (
 type Token struct {
 	tt  string
 	val string
+}
+
+type BinOp struct {
+	left  *BinOp
+	root  Token
+	right *BinOp
+}
+type NumNode struct {
+	token Token
+}
+
+type Parser struct {
+	cur    int
+	tokens []Token
 }
 
 func lexer(s string) []Token {
@@ -58,6 +61,44 @@ func getNum(i *int, s string) string {
 	return res
 }
 
-func parser() {
+func (p *Parser) next() Token {
+	if p.cur >= len(p.tokens) {
+		log.Fatal("EOF reached!")
+	}
+	t := p.tokens[p.cur]
+	p.cur++
+	return t
+}
+func (p *Parser) match(reqTokens []Token, errMsg string) bool {
+	t := p.next()
+	for _, reqToken := range reqTokens {
+		if reqToken == t {
+			return true
+		}
+	}
+	log.Fatal(errMsg)
+	return false
+}
+
+func (p *Parser) factor() BinOp {
+	t := p.tokens[p.cur]
+	if t.tt == num {
+		p.next()
+		return
+	}
+}
+
+func (p *Parser) parse() BinOp {
+
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("♪ ")
+		inp, _, _ := reader.ReadLine()
+		tokens := lexer(string(inp))
+		fmt.Println(tokens)
+	}
 
 }
